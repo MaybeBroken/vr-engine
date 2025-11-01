ba3ca43b80828896f84d69b297ce97d5c3f2399d
MaybeBroken <davidsponseller123@gmail.com>
2025-11-01 12:37:08 -0600

    Enhance compile.py with project template parsing functionality; add headers.py file

--- compiler/compile.py
+++ compiler/compile.py
@@ -1,3 +1,27 @@
+"""
+This is a program madfe to compile VR projects into C++ and generate the necessary OpenXR/Android Studio project
+"""
+
+import glob
+import pathlib
+from shutil import copy
+
+try:
+    XR_PROJECTS_ROOT = pathlib.Path("../XrSamples/").resolve()
+    TEMPLATE_ROOT = XR_PROJECTS_ROOT / "VrEngine"
+    TEMPLATE_EXCLUDES = [
+        TEMPLATE_ROOT / "Project/Android/.cxx/",
+        TEMPLATE_ROOT / "Project/Android/build/",
+        TEMPLATE_ROOT / "Project/Android/.gradle/",
+    ]
+except Exception:
+    exit(
+        "Could not resolve template root path. Your installation is most like corrupted, or you have screwed with the file structure."
+    )
+
+PROJECTS_DIR = pathlib.Path("./projects/").resolve()
+
+
 class Indicators:
     INCLUDES_ENTRY = "INCLUDES_ENTRY"
     INCLUDES_EXIT = "INCLUDES_EXIT"
@@ -64,6 +88,42 @@ class Indicators:
             }
 
 
+def parse_template_project(context: "Compiler"):
+    NEW_PROJECT_DIR = XR_PROJECTS_ROOT / context.project.name
+    template_files = glob.glob(str(TEMPLATE_ROOT), recursive=True)
+    for file_path in template_files:
+        file_path_obj = pathlib.Path(file_path)
+        if any(
+            str(file_path_obj).startswith(str(exclude)) for exclude in TEMPLATE_EXCLUDES
+        ):
+            continue
+        relative_path = file_path_obj.relative_to(TEMPLATE_ROOT)
+        new_file_path = NEW_PROJECT_DIR / relative_path
+        new_file_path.parent.mkdir(parents=True, exist_ok=True)
+        copy(file_path_obj, new_file_path)
+
+
 class Parser:
     def __init__(self):
         self.indicators = Indicators.all_indicators()
+
+
+class Project:
+    def __init__(self, name):
+        self.name = name
+
+
+class Compiler:
+    def __init__(self, project: Project):
+        self.project: Project = project
+        self.parser = Parser()
+
+    def compile(self):
+        parse_template_project(self)
+        pass
+
+
+if __name__ == "__main__":
+    sample_project = Project("SampleProject")
+    compiler = Compiler(sample_project)
+    compiler.compile()
